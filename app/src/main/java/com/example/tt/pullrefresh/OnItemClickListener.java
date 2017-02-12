3c2ef82da51c7df0b115d4d4ed205873331cb1ae
Joe <lovejjfg@163.com>
2017-02-12 12:11:44 +0800

    remove files

--- app/src/main/java/com/example/tt/pullrefresh/OnItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.tt.pullrefresh;
-
-import android.view.View;
-import android.widget.ImageView;
-
-public interface OnItemClickListener {
-    void onItemClick(View itemView, ImageView image, int id);
-}
